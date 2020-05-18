a2728fc186c415779a380c74a900565d4404f4f0
Sipan Calli <sipan.calli@calliconsulting.se>
2020-05-18 20:30:23 +0200

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Sipan on 2020-05-18.
+//  Copyright © 2020 Sipan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
